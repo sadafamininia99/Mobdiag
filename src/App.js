44ec4ba02fb1f7107ab88e8724b4a7fce2e508e2
sadaf <aminicomputer96@gmail.com>
2024-01-12 16:46:07 +0330

    MobDiag test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <iframe
+          src="https://giphy.com/embed/vSbW8dAA1n516fYcbm"
+          width="396"
+          height="480"
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
+        🥑 MOBDIAG IS LOADING ...
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href="https://giphy.com/gifs/passion-nfldance-inzonedance-vSbW8dAA1n516fYcbm"></a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
