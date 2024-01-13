bbf82b07b46ac3679218f44ac47eecc8ca9b0f2e
sadaf <aminicomputer96@gmail.com>
2024-01-13 23:37:06 +0330

    mobiag text

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <iframe
-          src="https://giphy.com/embed/vSbW8dAA1n516fYcbm"
-          width="396"
-          height="480"
-          frameBorder="0"
-          class="giphy-embed"
-          allowFullScreen
-        ></iframe>
-        🥑 MOBDIAG IS LOADING ...
-        <p>
-          <a href="https://giphy.com/gifs/passion-nfldance-inzonedance-vSbW8dAA1n516fYcbm"></a>
-        </p>
+        MOBDIAG IS LOADING ....🥑
+        <p></p>
       </header>
     </div>
   );
